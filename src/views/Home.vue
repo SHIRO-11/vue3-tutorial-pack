d17ebc8eb24aa61cab820ba915be9d75b7df1ebe
史朗出口 <howaito01@gmail.com>
2021-07-12 11:18:04 +0900

    add:vue-routerを追加しました。

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <div id="nav">
+            <ul>
+                <li><router-link to="/DraggableTest">DraggableTest</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    components: {},
+};
+</script>
+
+<style scoped>
+#nav {
+    padding: 30px;
+}
+
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+    color: #42b983;
+}
+
+ul {
+    display: inline-block;
+    text-align: left;
+}
+</style>
