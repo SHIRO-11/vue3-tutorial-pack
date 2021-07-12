0c0224c0fb21e13397c873624a371a61225e1b8d
史朗出口 <howaito01@gmail.com>
2021-07-12 12:27:46 +0900

    update：createWebHashHistoryにしました。

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import DraggableTest from "../views/DraggableTest";
 
@@ -24,6 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
+    history: createWebHashHistory(process.env.BASE_URL),
     routes,
 });
 
